d1085bb8d47fcf53529fdb027f629a8c55236ce6
GeXiaoguo <xiaoguo_ge@yahoo.com>
2018-12-07 22:45:53 +1100

    fixed show result

--- src/components/ResultDisplay.js
+++ src/components/ResultDisplay.js
@@ -2,7 +2,17 @@ import React , {Component} from 'react';
 import '../App.css';
 
 class ResultDisplay extends Component {
-   
+    resultShow = (score) => {
+        switch (score) {
+            case score >= 90:
+                return "Exellent";
+            case score >= 60 && score < 90:
+                return "Good Work";
+            default:
+                return "Work Harder";
+            }
+    }
+
     render(){
         let array=[];
         for(let i=0; i<this.props.unknownWordsList.length;i++){
@@ -10,7 +20,8 @@ class ResultDisplay extends Component {
         }
 
         return <div>
-                <p> Your score is {this.props.score}</p>
+                <p> Your score is {this.props.score.toFixed(2)} %</p>
+                 <p>{this.resultShow(this.props.score)}</p>
                 <p> The words you don't know are </p>
                 <div className="array1">{array}</div>
             </div>
